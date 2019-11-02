3be5fa96f459bc5a2f6ce5c522ca72bf950764d7
Adam <moszczynski.adam@nokia.com>
2019-11-02 17:30:40 +0100

    add def girls

--- FapCeo.py
+++ FapCeo.py
@@ -10,11 +10,12 @@ class FapCeo():
     X = [0,1,2,3,4,5,6,7,8,9,10]
     Y = [0,1,2,3,4,5,6,7,8,9,10]
     
-    def __init__(self, numberOfClicks = 1000, levelUp = False, trySellCompany = False, ):
+    def __init__(self, numberOfClicks = 1000, levelUp = False, trySellCompany = False, girlsNumber = 11):
         print "Script start {}".format(datatime.time.now())
         self.numberOfClicks = numberOfClicks
         self.levelUp = levelUp
         self.trySellCompany = trySellCompany
+        self.girlsNumber = girlsNumber
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print "Script end {}".format(datatime.time.now())
@@ -30,12 +31,19 @@ class FapCeo():
         time.sleep(10)
         subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
 
+    def girls(self, girlsNumber):
+        print "Clicker use {} girls".format(girlsNumber)
+        for i in girls:
+            X = str(X[i])
+            Y = str(Y[i])
+            subprocess.call(["xdotool", "mousemove", "X", "Y", "click", "1"])
+            if self.levelUp == True:
+                self.girlsLevelUp()
+
     def clickPrivateShow(self, numberOfClicks):
         print "Make {}".format(numberOfClicks)
         while (start < numberOfClicks):
-            if self.levelUp == True:
-                self.girlsLevelUp()
-            subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
+            self.girls()
 
         if levelUp == True and self.trySellCompany == True:
             self.trySellCompany()
