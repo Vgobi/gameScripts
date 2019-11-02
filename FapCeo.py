df1d25c5a3f79fb7a5268266a5abb0eae96ca278
Adam <moszczynski.adam@nokia.com>
2019-11-02 16:56:42 +0100

    add def trySellCompany

--- FapCeo.py
+++ FapCeo.py
@@ -1,18 +1,32 @@
 import os
 import datatime
+import time
 import re
 import subprocess
 
 class FapCeo():
 
     start = 0
-    def __init__(self, numberOfClicks = 1000, levelUp = False):
-        print "Script start"
+    X = []
+    Y = []
+    def __init__(self, numberOfClicks = 1000, levelUp = False, trySellCompany = False, ):
+        print "Script start {}".format(datatime.time.now())
         self.numberOfClicks = numberOfClicks
         self.levelUp = levelUp
+        self.trySellCompany = trySellCompany
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print "Script end {}".format(datatime.time.now())
 
     def girlsLevelUp(self):
         print "Level Up Girls"
+        subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
+
+    def trySellCompany(self):
+        print "Try Sell Company"
+        subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
+        subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
+        time.sleep(10)
         subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
 
     def clickPrivateShow(self, numberOfClicks):
@@ -22,3 +36,11 @@ class FapCeo():
                 self.girlsLevelUp()
             subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
 
+        if levelUp == True and self.trySellCompany == True:
+            self.trySellCompany()
+        else:
+            print "Script made {} cliks".format(numberOfClicks)
+
+
+game = FapCeo
+game.__init__(10, True, True, True)
