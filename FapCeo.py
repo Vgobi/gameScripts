c9bfa1f69d9db37655c70101e982683acf9e9023
Adam <moszczynski.adam@nokia.com>
2019-11-03 11:52:24 +0100

    First working version

--- FapCeo.py
+++ FapCeo.py
@@ -10,16 +10,12 @@ Y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 class FapCeo():
     
-    def __init__(self, numberOfClicks = 1000, levelUp = False, trySellCompany = False, girlsNumber = 11):
-    #def __init__(self):
+    def __init__(self, numberOfClicks = 10, levelUp = False, trySellCompany = False, girlsNumber = 11):
         print ("Script start {}".format(datetime.datetime.now()))
         self.numberOfClicks = numberOfClicks
         self.levelUp = levelUp
         self.trySellCompany = trySellCompany
         self.girlsNumber = girlsNumber
-        self.girls(self.girlsNumber)
-        self.clickPrivateShow(self.numberOfClicks)
-
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print ("Script end {}".format(datetime.datetime.now()))
@@ -35,29 +31,31 @@ class FapCeo():
         time.sleep(10)
       # subprocess.call(["xdotool", "mousemove", "945", "132", "click", "1"])
 
-    def girls(self, girlsNumber):
-        print ("Clicker use {} girls".format(girlsNumber))
-        #for i in girls:
-         #   X = str(X[i])
-          #  Y = str(Y[i])
+    def girls(self):
+        print ("Clicker use {} girls".format(self.girlsNumber))
+        for i in X:
+            xX = str(X[i])
+            yY = str(Y[i])
+            print ("X: {} Y: {}".format(xX,yY))
           #  subprocess.call(["xdotool", "mousemove", "X", "Y", "click", "1"])
-        print (X)
-        print (Y)
+
             #if self.levelUp == True:
              #   self.girlsLevelUp()
 
-    def clickPrivateShow(self, numberOfClicks):
-        print ("Make {} Clicks".format(numberOfClicks))
-        while (start < numberOfClicks):
+    def clickPrivateShow(self):
+        start = 0
+        print ("Make {} Clicks".format(self.numberOfClicks))
+        while (start < self.numberOfClicks):
             self.girls()
+            start = start + 1
 
-        if levelUp == True and self.trySellCompany == True:
+        if self.levelUp == True and self.trySellCompany == True:
             self.trySellCompany()
         else:
-            print ("Script made {} cliks".format(numberOfClicks))
+            print ("Script made {} cliks".format(self.numberOfClicks))
 
 def main():
-    game = FapCeo()
-    game.clickPrivateShow(10)
+    game = FapCeo(10)
+    game.clickPrivateShow()
 
 main()
