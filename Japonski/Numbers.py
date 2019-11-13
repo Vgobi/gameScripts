1241f2cae8c2313f77ffebb6ff04636918a9a298
Adam <moszczynski.adam@nokia.com>
2019-11-13 11:13:17 +0100

    add exception for poda liczbe value

--- Japonski/Numbers.py
+++ Japonski/Numbers.py
@@ -2,11 +2,17 @@ import random
 
 def ten():
 
+    
     numbers={0:"zero", 1:"ichi", 2:"ni", 3:"san", 4:"yon", 5:"go", 6:"roku", 7:"nana", 8:"hachi", 9:"kyuu",10: "juu"}
     losowanie = random.randrange(11)
     print(numbers[losowanie])
-    poda_liczbe = input("Podaj cyfre: ")
-    if int(poda_liczbe) is int(losowanie):
+    while 1:
+        try:
+            poda_liczbe = int(input("Podaj cyfre: "))
+            break
+        except ValueError:
+            print("Prosze podać cyfre")
+    if poda_liczbe is int(losowanie):
         print("Zgadłeś")
     else:
         print("dupa")
