40bd664272aa3c3fdf901e4cea457c4ba9a5ab7e
Adam <moszczynski.adam@nokia.com>
2019-11-13 15:15:57 +0100

    add guess in_row counter

--- Japonski/Numbers.py
+++ Japonski/Numbers.py
@@ -3,19 +3,24 @@ import random
 def ten():
 
     numbers={0:"zero", 1:"ichi", 2:"ni", 3:"san", 4:"yon", 5:"go", 6:"roku", 7:"nana", 8:"hachi", 9:"kyuu",10: "juu"}
-
+    in_row = 0
     while 1:
+
         losowanie = random.randrange(11)
         print(numbers[losowanie])
         try:
             poda_liczbe = int(input("Podaj cyfre: "))
         except ValueError:
             print("Prosze podać cyfre")
-
+        
         if poda_liczbe is int(losowanie):
+
             print("Zgadłeś")
+
         else:
             print("dupa")
             break
+        in_row = in_row + 1
+    print("Zgadles {} pod rzad".format(in_row))
 
 ten()
