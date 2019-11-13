4fe50da553fbb99f89a1edd5291e0788a30f4361
Adam <moszczynski.adam@nokia.com>
2019-11-13 13:23:36 +0100

    infinity while for long game (to first lose)

--- Japonski/Numbers.py
+++ Japonski/Numbers.py
@@ -2,19 +2,20 @@ import random
 
 def ten():
 
-    
     numbers={0:"zero", 1:"ichi", 2:"ni", 3:"san", 4:"yon", 5:"go", 6:"roku", 7:"nana", 8:"hachi", 9:"kyuu",10: "juu"}
-    losowanie = random.randrange(11)
-    print(numbers[losowanie])
+
     while 1:
+        losowanie = random.randrange(11)
+        print(numbers[losowanie])
         try:
             poda_liczbe = int(input("Podaj cyfre: "))
-            break
         except ValueError:
             print("Prosze podać cyfre")
-    if poda_liczbe is int(losowanie):
-        print("Zgadłeś")
-    else:
-        print("dupa")
+
+        if poda_liczbe is int(losowanie):
+            print("Zgadłeś")
+        else:
+            print("dupa")
+            break
 
 ten()
