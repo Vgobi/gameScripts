26a8ed9e59d9a2f996364a7672ae342a0af23e3f
Vgobi <adam.moszczynski1@gmail.com>
2019-12-10 20:29:24 +0100

    some changes

--- Japonski/hiragana.py
+++ Japonski/hiragana.py
@@ -2,13 +2,13 @@ import random
 from PIL import Image
 import time
 
-hiragana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se", "so", "ta", "ti", "tu", "te",
-            "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo",
-            "ra", "ri", "ru", "re", "ro", "wa", "o", "n"]
+hiragana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "tci", "tsu",
+            "te","to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ma", "mi", "mu", "me", "mo", "ya", "yu",
+            "yo", "ra", "ri", "ru", "re", "ro", "wa", "o", "n"]
 print(len(hiragana))
 i = 0
-
-while 1:
+number = int(input("number of sights: "))
+while i < number:
 
     print("{:3}|{}".format(i, hiragana[random.randrange(0, len(hiragana))]))
 #    img = Image.open("a.jpg")
